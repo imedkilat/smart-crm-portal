a271de7fd7e36718e677bcdc5f3828e7129a442e
imedkilat <48803608+imedkilat@users.noreply.github.com>
2026-08-17 10:20:31 +0800

    Wire login and password recovery routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import Login from './pages/Login'
+import ResetPassword from './pages/ResetPassword'
 import './styles.css'
+
+function currentRoute() {
+  return window.location.pathname.replace(/\/+$/, '') || '/'
+}
+
+const route = currentRoute()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {route === '/login' ? (
+      <Login onSignedIn={() => window.location.assign('/')} />
+    ) : route === '/reset-password' ? (
+      <ResetPassword />
+    ) : (
+      <App />
+    )}
   </StrictMode>,
 )
