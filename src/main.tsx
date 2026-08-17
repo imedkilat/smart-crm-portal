b1fe4f607828644833229b44af9ba2a07995e66b
imedkilat <48803608+imedkilat@users.noreply.github.com>
2026-08-18 02:45:27 +0800

    Mount global search inside app layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode, useEffect, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import GlobalSearch from './components/GlobalSearch'
 import Login from './pages/Login'
 import ResetPassword from './pages/ResetPassword'
 import { signOut } from './lib/auth'
@@ -118,7 +117,6 @@ function AuthRouter() {
   return (
     <>
       <App />
-      <GlobalSearch />
       <button
         className="session-signout"
         type="button"
